c8ada19545bc9240e95376abe90d0252468db757
ShujiOba <shuji365630@gmail.com>
2019-02-15 15:07:51 +0900

    update

--- models/user.js
+++ models/user.js
@@ -1,13 +1,16 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
-  }, {});
-  User.associate = function (models) {
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE
+    },
+    {}
+  );
+  User.associate = function(models) {
     // associations can be defined here
   };
   return User;
--- seeders/20190206165558-users.js
+++ seeders/20190206165558-users.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const csv = require('../csv');
 
 module.exports = {
